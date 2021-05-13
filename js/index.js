e5efe83c056e83775e33de741ca7cea462afee58
Duck2693 <patricio.fredes.g@gmail.com>
2021-05-13 19:51:15 -0400

    Agregue la validacion, el icono de oferta

--- js/index.js
+++ js/index.js
@@ -15,29 +15,56 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
 
-//horario
-//const cargarHorario = () =>{
-//  let horarioSelect = document.querySelector("#horario-select");
-//   for(let i = 0; i < ca; ++i){
-//      let comida = document.createElement("-")
-//      let desayuno = document.createElement("Desayuno");
-//      let almuerzo = document.createElement("Almuerzo");
-//      let once = document.createElement("Once");
-//      let cena = document.createElement("Cena");
-//      comida.appendChild(desayuno);
-//      comida.appendChild(almuerzo);
-//      comida.appendChild(once);
-//      comida.appendChild(cena);
-//      horarioSelect.appendChild(comida); 
-//}
-//};
 
-//validar valor
-
-//Generar tabla
-//nombre, horario, valor, descripcion, oferta
 const Orden = [];
 
+//Validacion
+function validacion(){
+    selector = document.querySelector("#horario-select").value;
+    val = document.querySelector("#valor-int").value;
+    if(selector == "Desayuno"){
+        if(val >= 1000 && val <= 10000){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(selector == "Almuerzo"){
+        if(val >= 10000 && val <= 20000){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(selector == "Once"){
+        if(val >= 5000 && val <= 15000){
+            return true;
+        }else{
+            return false;
+        }
+    }else if(selector == "Cena"){
+        if(val > 15000){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+const iconoferta = () =>{
+    validacion();
+    if(selector == "Desayuno" && val < 5000){
+        return true;
+    }else if(selector == "Almuerzo" && val < 15000){
+        return true;
+    }else if(selector == "Once" && val <10000){
+        return true;
+    }else if(selector == "Cena" && val <20000){
+        return true;
+    }else{
+        return false;
+    }
+};
+
+//cargar tabla
 const cargarTabla = () =>{
     let tbody = document.querySelector("#tabla-tbody");
     tbody.innerHTML = "";
@@ -49,29 +76,45 @@ const cargarTabla = () =>{
         let tdHorario = document.createElement("td");
         tdHorario.innerText = o.horario;
         let tdValor = document.createElement("td");
-        tdValor.innerText = o.valor;
+        tdValor.innerText = ("$" + o.valor);
         let tdDesc = document.createElement("td");
         tdDesc.innerHTML = o.descripcion;
-
+        let tdOferta = document.createElement("td");
+        let icono = document.createElement("i");
+        if (iconoferta() == true){
+            icono.classList.add("fas","fa-hand-holding-usd","fa-2x");
+            console.log("hola")
+        }else{
+            icono.classList.add("fas","fa-receipt","fa-2x");
+            console.log("chao")
+        }
+        tdOferta.appendChild(icono);
         tr.appendChild(tdNombre);
         tr.appendChild(tdHorario);
         tr.appendChild(tdValor);
         tr.appendChild(tdDesc);
+        tr.appendChild(tdOferta);
         tbody.appendChild(tr);
     }
 };
+
+//boton registrar
 document.querySelector("#registrar-btn").addEventListener("click", () => {
     let nombre = document.querySelector("#nombre-txt").value;
     let horario = document.querySelector("#horario-select").value;
     let valor = document.querySelector("#valor-int").value;
     let descripcion = tinymce.get("descripcion-txt").getContent();
-
     let pedido = {};
     pedido.nombre = nombre;
     pedido.horario = horario;
     pedido.valor = valor;
     pedido.descripcion = descripcion;
-    Orden.push(pedido);
+    validacion();
+    if (validacion() == true){
+        Orden.push(pedido);
+        Swal.fire("Registro de menu realizado","","success")
+    }else{
+        Swal.fire("El valor esta fuera del rango de horario","","error")
+    }
     cargarTabla();
-    Swal.fire("Registro de menu realizado","","success")
 });
